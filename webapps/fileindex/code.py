4eb9e50501d2132fcb574bba017bf46fe7519988
gunny26 <arthur.messner@gmail.com>
2016-01-08 00:15:01 +1030

    using only GET mothod and multiplexing

--- webapps/fileindex/code.py
+++ webapps/fileindex/code.py
@@ -31,23 +31,70 @@ class FileIndex(object):
         return os.path.join(STORAGE_DIR, args)
 
     def GET(self, args):
+        params = args.split("/")
+        method = None
+        method_args = None
+        if len(params) == 1:
+            method = params[0]
+            method_args = ()
+        else:
+            method = params[0]
+            method_args = params[1:]
+        logging.info("calling method %s", method)
+        data = web.data()
+        if method == "get":
+            return self.get(method_args, data)
+        elif method == "exists":
+            return self.exists(method_args, data)
+        elif method == "stats":
+            return self.stats(method_args, data)
+        elif method == "put":
+            return self.put(method_args, data)
+        elif method == "mkdir":
+            return self.mkdir(method_args, data)
+        elif method == "listdir":
+            return self.listdir(method_args, data)
+        elif method == "delete":
+            return self.delete(method_args, data)
+        else:
+            return "unknown method %s called" % method
+
+    def get(self, args, data):
         """
-	    get block stored in blockstorage directory with hash
+	get block stored in blockstorage directory with hash
+
+        if arguments end with / a directory listing will be served,
+        otherwise the content of the specific file will be returned
         """
-        filename = self.__get_filename(args)
+        filename = self.__get_filename("/".join(args))
         if os.path.isfile(filename):
             web.header('Content-Type', 'application/octet-stream')
-            return json.dumps(open(filename, "rb").read())
+            return open(filename, "rb").read()
+        elif os.path.isdir(filename):
+            return json.dumps(os.listdir(filename))
         else:
             logging.error("File %s does not exist", filename)
             web.notfound()
 
-    def EXISTS(self, args):
+    def listdir(self, args, data):
+        """
+	get block stored in blockstorage directory with hash
+
+        if arguments end with / a directory listing will be served,
+        otherwise the content of the specific file will be returned
+        """
+        filename = self.__get_filename("/".join(args))
+        if os.path.isdir(filename):
+            return json.dumps(os.listdir(filename))
+        else:
+            logging.error("File %s does not exist", filename)
+            web.notfound()
+
+    def exists(self, args, data):
         """
         check if block with digest exists
         """
-	logging.info("EXISTS called with %s", args)
-        filename = self.__get_filename(args)
+        filename = self.__get_filename("/".join(args))
         if os.path.isfile(filename):
             logging.info("found file %s", filename)
             web.ctx.status = '200 file exists'
@@ -57,40 +104,64 @@ class FileIndex(object):
         else:
             web.notfound()
 
-    def POST(self, args):
+    def stats(self, args, data):
         """
-        put data into storage
+        return file stats of stored file
         """
-        #logging.debug("POST called")
-        filename = self.__get_filename(args)
-        if len(web.data()) != 0:
-            checksum = json.loads(web.data())
-            if not os.path.isfile(filename):
-                try:
-                    json.dump(checksum, open(filename, "wb"))
-                    web.ctx.status = '201 metadata stored'
-                except TypeError:
-                    os.unlink(filename)
-            else:
-                 try:
-                    json.dump(checksum, open(filename, "wb"))
-                    web.ctx.status = '202 metadata overwritten'
-                    logging.info("file %s overwritten", filename)
-                 except TypeError:
-                    pass
-        else:
-            logging.info("creating directory %s", filename)
-            os.mkdir(filename)
-            web.ctx.status = '203 directory created'
-    PUT = POST
+        filename = self.__get_filename("/".join(args))
+        stat = os.stat(filename)
+        ret_data = {
+            "st_atime" : stat.st_atime,
+            "st_mtime" : stat.st_mtime,
+            "st_ctime" : stat.st_ctime,
+        }
+        return json.dumps(ret_data)
 
-    def DELETE(self, args):
+    def put(self, args, data):
+        """
+        store checksum in file named
+
+        URL: /filename
+        data: checksum (output of hexdigest())
+
+        if no checksum is given, a directory will be created
+        """
+        filename = self.__get_filename("/".join(args))
+        assert len(data) > 0
+        checksum = web.data()
+        if not os.path.isfile(filename):
+            try:
+                open(filename, "wb").write(checksum)
+                web.ctx.status = '201 checksum stored'
+            except IOError as exc:
+                logging.exception(exc)
+                os.unlink(filename)
+        else:
+            open(filename, "wb").write(checksum)
+            web.ctx.status = '202 checksum overwritten'
+            logging.info("file %s overwritten", filename)
+
+    def mkdir(self, args, data):
+        """
+        store checksum in file named
+
+        URL: /filename
+        data: checksum (output of hexdigest())
+
+        if no checksum is given, a directory will be created
+        """
+        filename = self.__get_filename("/".join(args))
+        logging.info("creating directory %s", filename)
+        os.mkdir(filename)
+        web.ctx.status = '203 directory created'
+
+    def delete(self, args, data):
         """
         delete block with md5checksum given
 
         the block should only be deleted if not used anymore in any FileStorage
         """
-        filename = self.__get_filename(args)
+        filename = self.__get_filename("/".join(args))
         if os.path.isfile(filename):
             os.unlink(filename)
             web.ctx.status = '201 metadata deleted'
