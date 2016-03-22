8c7be831879f7defe079207da99cddeced8cca47
Arthur Messner <arthur.messner@gmail.com>
2016-03-22 23:42:06 +0100

    better reporting during scan

--- client/filestorage_check.py
+++ client/filestorage_check.py
@@ -16,20 +16,24 @@ if __name__ == "__main__":
     fs = FileStorageClient(None)
     bs = BlockStorageClient(None)
     existing_blocks = set(bs.ls())
+    logging.info("found %d blocks in BlockStorage", len(existing_blocks))
     used_blocks = set()
-    for checksum in fs.ls():
-        assert fs.exists(checksum)
-        logging.info("checking file with checksum %s", checksum)
+    existing_files = fs.ls()
+    logging.info("found %d files in FileStorage", len(existing_files))
+    percent = 0.0
+    starttime = time.time()
+    for counter, checksum in enumerate(existing_files):
+        logging.debug("checking file with checksum %s", checksum)
         metadata = fs.get(checksum)
         for blockchecksum in metadata["blockchain"]:
             if not blockchecksum in existing_blocks:
                 print "Block %s of file %s are missing" % (blockchecksum, checksum)
             else:
                 used_blocks.add(blockchecksum)
-        #md5 = hashlib.md5()
-        #data = bs.get(checksum)
-        #md5.update(data)
-        #assert md5.hexdigest() == checksum
+        if (100.0 * counter/ len(existing_files)) > (percent + 1.0):
+            percent = 100.0 * counter/ len(existing_files)
+            logging.info("%0.2f %% done in %0.2f s (%d of %d file scanned)", percent, time.time()-starttime, counter, len(existing_files))
+            starttime = time.time()
     logging.info("used blocks %d", len(used_blocks))
     unused_blocks = [blockchecksum for blockchecksum in existing_blocks if blockchecksum not in used_blocks]
     logging.info("unused blocks %d", len(unused_blocks))
