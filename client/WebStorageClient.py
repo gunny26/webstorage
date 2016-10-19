a2722d8f1db2d3f2ff19c3ad5949942977de06dd
gunny26 <arthur.messner@gmail.com>
2016-10-19 17:12:46 +0200

    hack to work on earlier python 3 version

--- client/WebStorageClient.py
+++ client/WebStorageClient.py
@@ -267,7 +267,11 @@ class FileStorageClient(WebAppClient):
         logging.debug("GET %s", url)
         res = self.session.get(url)
         if res.status_code == 200:
-            self.__cache_checksums = set(res.json)
+            # hack to work also on earlier versions of python 3
+            try:
+                self.__cache_checksums = set(res.json)
+            except TypeError:
+                self.__cache_checksums = set(res.json())
         else:
             logging.error("Failure to get stored checksum from FileStorage Backend, status %s", res.status_code)
  
