634f6a3de6ae14b0142135f535ba67b0b35c3acd
gunny26 <arthur.messner@gmail.com>
2018-11-26 17:02:58 +0100

    chunked transfer encoding

--- client/BlockStorageClient.py
+++ client/BlockStorageClient.py
@@ -44,9 +44,26 @@ class BlockStorageClient(WebStorageClient):
             self._checksums = set(self._get_json())
         return self._checksums
 
+    def _get_chunked(self, method, data=None):
+        """
+        call url and received chunked content to yield
+        """
+        url = "/".join((self._url, method))
+        if data is not None:
+            print("adding search parameters : %s", data)
+        print("calling %s" % url)
+        res = requests.get(url, params=data, headers=self.__headers, proxies=self.__proxies, stream=True)
+        logging.debug("got Status code : %s", res.status_code)
+        if res.status_code == 200:
+            return res
+        elif res.status_code == 404:
+            raise KeyError("HTTP 404 received")
+        else:
+            raise Exception("got status %d for call to %s" % (res.status_code, url))
+
     def put(self, data, use_cache=False):
         """put some arbitrary data into storage"""
-        assert len(data) <= self.blocksize
+        assert len(data) <= self.blocksize # assure maximum length
         checksum = self._blockdigest(data)
         if use_cache and checksum in self.checksums:
             self._logger.debug("202 - skip this block, checksum is in list of stored checksums")
@@ -61,12 +78,16 @@ class BlockStorageClient(WebStorageClient):
             return res.text, res.status_code
 
     def get(self, checksum):
-        """get data defined by hexdigest from storage"""
+        """
+        get data defined by hexdigest from storage
+        """
         res = self._request("get", checksum)
         return res.content
 
     def get_verify(self, checksum):
-        """get data defined by hexdigest from storage"""
+        """
+        get data defined by hexdigest from storage and verify received block to check integrity
+        """
         res = self._request("get", checksum)
         data = res.content
         if checksum != self._blockdigest(data):
@@ -78,21 +99,18 @@ class BlockStorageClient(WebStorageClient):
         exists method if caching is on
         if the searched checksum is not available, the filestorage backend is queried
         """
-        if checksum in self._checksums:
+        if checksum in self._checksums: # check local cache first
             return True
         if self._exists(checksum):
             self._checksums.add(checksum)
             return True
         return False
 
-#    def exists(self, checksum):
-#        """
-#        exists method if caching is on
-#        if the searched checksum is not available, the backend is queried
-#        """
-#        try:
-#            if checksum in self._checksums or self._request("options", checksum).status_code == 200:
-#                return True
-#        except KeyError:
-#            pass
-#        return False
+    def meta(self):
+        """
+        receive meta information of any checksum stored in bockstorage
+        """
+        res = self._get_chunked("meta")
+        for chunk in res.iter_lines(decode_unicode=True):
+            yield json.loads(chunk)
+
